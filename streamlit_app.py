842e779923a4892ccbaa5a7119e33aa56c944be9
Ketul Patel <ketulp19120@gmail.com>
2026-07-25 23:26:18 -0400

    - updated header files for streamlit compatibility

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
-from keras.preprocessing import image
-from keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
+import tensorflow as tf
 import numpy as np
-import tensorflow
+
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
+
+
 
 # Function to clear uploaded image
 def clear_image():
